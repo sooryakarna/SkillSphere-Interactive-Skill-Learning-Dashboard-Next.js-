aa8aa7c37b421e257c7683bdf3c34cb3fe2dde31
Soorya <soorisoorya666@gmail.com>
2026-03-14 22:41:46 +0530

    Create next.js

---
+++ next.js
@@ -0,0 +1,21 @@
+export default function SkillCard(){
+
+return(
+
+<div className="bg-white shadow-lg p-6 rounded-xl">
+
+<h2 className="text-xl font-bold">Machine Learning</h2>
+
+<div className="w-full bg-gray-200 h-4 rounded mt-3">
+
+<div className="bg-green-500 h-4 rounded w-2/3"></div>
+
+</div>
+
+<p className="mt-2 text-sm">Progress: 65%</p>
+
+</div>
+
+)
+
+}
